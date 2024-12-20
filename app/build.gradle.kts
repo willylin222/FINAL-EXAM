622871209380fde476ba284d7a9151d945b15588
11110256 <11110256@a.com>
2024-12-21 02:35:31 +0800

    設定viewbinding
    完成viewbinding設定

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.afinal"
     compileSdk = 33
 
+    buildFeatures{
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.afinal"
         minSdk = 28
--- app/src/main/java/com/example/afinal/MainActivity.kt
+++ app/src/main/java/com/example/afinal/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.afinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.afinal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bd : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bd = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bd.root)
     }
 }
